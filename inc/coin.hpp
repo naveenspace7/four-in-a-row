057708e2138f96e7549716983cab7844d497798d
naveen chakravarthy <naveen.j_94@outlook.com>
2020-08-29 18:59:51 +0200

    Make COIN RADIUS public

--- inc/coin.hpp
+++ inc/coin.hpp
@@ -11,11 +11,11 @@ const sf::Color RED_COLOR = sf::Color(250, 26, 26);
 class Coin : public sf::CircleShape
 {
 private:
-    static constexpr float COIN_RADIUS = 50.f;
     static const int m_border_shade  =  2;
     static constexpr float m_border_perc = 10;
 
 public:
+    static constexpr float COIN_RADIUS = 50.f;
     Coin(sf::Color main_color = sf::Color(100,100,100));
 };
 
